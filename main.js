bc6260ab0d2199d451fdd51414fadaf3a671596d
teeya123 <73390143+teeya123@users.noreply.github.com>
2021-05-01 06:07:48 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var song="";
+song="";
 leftWristX=0;
 leftWristY=0;
 rightWristX=0;
@@ -21,6 +21,17 @@ poseNet.on('pose',gotPoses);
 }
 function draw(){
     image(video,0,0,600,500);
+    fill("#FF0000");
+    stroke("#FF0000");
+    if(scoreLeftWrist>0.2){
+    circle(leftWristX,leftWristY,20);
+    InNumberleftWristY=Number(leftWristY);
+    remove_decimals=floor(InNumberleftWristY);
+    leftWristY_divide_1000=remove_decimals/1000;
+    volume=leftWristY_divide_1000*2;
+    document.getElementById("volume").innerHTML="Volume="+volume;
+    song.setVolume(volume);
+    }
 }
 function play(){
     song.play();
@@ -36,6 +47,8 @@ function gotPoses(results)
     if(results.length>0) 
     {
         console.log(results);
+        scoreLeftWrist=results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist="+scoreLeftWrist);
         leftWristX=results[0].pose.leftWrist.x;
         leftWristY=results[0].pose.leftWrist.y;
         console.log("leftWristX=" + leftWristX+ "leftWristY="+leftWristY);
